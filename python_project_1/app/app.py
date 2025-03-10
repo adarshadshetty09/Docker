32481fca8d0f23086d4b96b2c5a2d54b7a67c4c5
adarshadshetty09 <adarshadshetty09@gmail.com>
2025-03-10 12:34:18 +0530

    Update

---
+++ python_project_1/app/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import redis
+import os
+
+app = Flask(__name__)
+
+# Connect to Redis
+cache = redis.Redis(host=os.getenv('REDIS_HOST', 'redis'), port=6379)
+
+@app.route('/')
+def hello():
+    count = cache.incr('visit')
+    return f'Hello, World! You have visited {count} times.'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
