0e5bff0aafd568383b18b3da4142e9fd20cf4116
adarshadshetty09 <adarshadshetty09@gmail.com>
2025-04-02 20:09:04 +0530

    Update

--- FRONTEND/my-react-app/src/App.js
+++ FRONTEND/my-react-app/src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 style={{ color: "cyan" }}>🚀 Welcome to My Dockerized React App! 🎉</h1>
+        <h2 style={{ color: "cyan" }}>🚀 Welcome to DevOps Engineer 🎉</h2>
         <p>Edit <code>src/App.js</code> and save to reload.</p>
         <button style={{ padding: "10px 20px", fontSize: "16px", cursor: "pointer" }}>
           Click Me
