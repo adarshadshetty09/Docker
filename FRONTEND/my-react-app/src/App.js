c70accfcc73f16593de8dcc06689c3170e5a11ea
adarshadshetty09 <adarshadshetty09@gmail.com>
2025-04-02 19:56:23 +0530

    Update

--- FRONTEND/my-react-app/src/App.js
+++ FRONTEND/my-react-app/src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p style={{ color: "yellow", fontWeight: "bold" }}>
+          ✅ Mount Check: This text should change instantly!
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
